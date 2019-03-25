0b8240278192a77e217c991a23cb95ac0529912d
squirrel <squirrelzt@126.com>
2019-03-25 13:11:30 +0800

    web

--- web/server/routes/main.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/main', function(req, res, next) {
-  res.render('main', { title: 'FTP MONITOR' });
-});
-
-module.exports = router;
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,74 +1,67 @@
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
-var pkg = require(path.join(process.cwd(), 'package.json'));
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  devtool: false,
-  cache:true,
-  resolve: {
-    modulesDirectories: ['node_modules', path.join(__dirname, '../node_modules')],
-    extensions: ['', '.js', '.jsx']
+    entry: {
+        index: './src/entry/index.js'
+	},
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+	},
+    plugins: [
+        new CleanWebpackPlugin(),
+    	new HtmlWebpackPlugin({
+            title: 'Output Management'
+		}),
+        new ManifestPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+	],
+    output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+	publicPath: '/'
   },
-  resolveLoader: {
-      modulesDirectories: ['node_modules', path.join(__dirname, '../node_modules')],
-  },
-  output: {
-    path: path.join(process.cwd(),'./dist/'),
-    filename: 'js/[name].js',
-    chunkFilename: 'js/[id].chunk.js',
-    publicPath: '/'
-  },
-  entry: [
-    './app/entry/index.js'
-  ],
   module: {
-    loaders: [{
-       test: /\.js?$/,
-       exclude: /node_modules/,
-       loader: 'es3ify',
-       query:{
-         plugins:['antd']
-       }
-     },{
-        test: /\.js?$/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react', 'stage-0'],
-          plugins: ['add-module-exports'],
-      },
-      exclude: /node_modules/,
-      include: __dirname
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader'
-    },{
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract('css?sourceMap!' + 'autoprefixer-loader!' + ('less?{"sourceMap":true,"modifyVars":' + JSON.stringify(pkg.theme || {}) + '}'))
-      }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract(
-        'css?sourceMap&-restructuring!' +
-            'postcss-loader!'+
-            'autoprefixer-loader! '
-      )
-    }, { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=application/font-woff' },
-        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=application/font-woff' },
-        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=application/octet-stream' },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=image/svg+xml' },
-        { test: /\.(png|jpg|jpeg|gif)(\?v=\d+\.\d+\.\d+)?$/i, loader: 'url?limit=10000&name=images/[name].[ext]' },
-        { test: /\.json$/, loader: 'json' }
-      ]
-  },
-
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common', 'js/common.js'),
-        new ExtractTextPlugin('css/[name].css', {
-            disable: false,
-            allChunks: true
-        }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
-}
+	  rules: [
+	    {
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader'
+				]
+		},{
+			test: /\.(png|svg|jpg|gif)$/,
+			use: [
+				'file-loader'
+			]
+		}, {
+			test: /\.(woff|woff2|eot|ttf|otf)$/,
+			use: [
+				'file-loader'
+				]
+		}, {
+			test: /\.(csv|tsv)$/,
+			use: [
+				'csv-loader'
+				]
+		}, {
+			test: /\.xml$/,
+			use: [
+				'xml-loader'
+				]
+		}, {
+	  		test: /\.js$/,
+			exclude: /node_modules/,
+			use: [
+				'babel-loader'
+			]
+		  }
+		]
+  }
+};
