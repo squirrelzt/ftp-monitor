a0b55c4f93bc4e33319398bfd13481ec57a90c95
squirrel <squirrelzt@126.com>
2019-03-26 16:41:05 +0800

    update

--- web/src/entry/index.js
+++ web/src/entry/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Home from '../component/home.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Nav from './../component/nav/nav.js';
+import Ftp from './../component/ftp/ftp.js';
 
 function component() {
   var element = document.createElement('pre');
@@ -8,13 +10,15 @@ function component() {
   return element;
 }
 
-// document.body.appendChild(component());
-
 let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
 document.body.appendChild(element);
 
-ReactDOM.render(
-    <Home></Home>,
-    document.getElementById('root')
+ReactDOM.render((
+  <Router>
+    <div>
+      <Route exact path="/nav" component = { Nav } />
+      <Route path="/ftp" component = { Ftp } />
+    </div>
+  </Router>
+),document.getElementById('root')
 );
-
