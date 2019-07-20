2a4c10f33c4522841d8653f58b765571fdded823
squirrel <squirrelzt@126.com>
2019-07-20 08:45:49 +0800

    antd Upload实现缩略图

--- web/src/component/pictureswall/PicturesWall.js
+++ web/src/component/pictureswall/PicturesWall.js
@@ -1,69 +1,81 @@
 import React, { Component } from 'react'
 import auth from './../../common/auth';
+import { Button, Upload } from 'antd';
 
 class PicturesWall extends Component {
     constructor(props) {
         super();
         this.state = {
-            previewVisible: false,
-            previewImage: '',
-            fileList: [
-              {
-                uid: '-1',
-                name: 'xxx.png',
-                status: 'done',
-                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-              },
-            ],
-          }
+          previewVisible: false,
+          previewImage: '',
+          fileList: [
+            {
+              uid: '-1',
+              name: 'xxx.png',
+              status: 'done',
+              url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            },{
+              uid: '-2',
+              name: 'java.png',
+              status: 'done',
+              url: 'http://127.0.0.1:8080/111111.jpg',
+            }
+          ],
+        }
     }
 
     componentWillMount(){
-        // let file = {
-        //     uid: '-1',
-        //     name: 'xxx.png',
-        //     status: 'done',
-        //     url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        // }
-        // let file = document.getElementById('imgId');
-        // let img = this.getBase64(file);
-        // this.setState({
-        //     previewImage: img
-        // })
-
-        // const reader = new FileReader();
-        //   reader.readAsDataURL(file);
-        //   reader.onload = (e) => {
-        //     this.setState({
-        //         previewImage: e.target.result
-        //     })
-        //   }
 
     }
-
+    handleChange = (evt) => {
+      // console.log('-----------------');
+      // console.log(evt);
+      this.setState({
+        fileList: evt.fileList
+      })
+      // console.log(this.state.fileList);
+    }
+    handlePreview = file => {
+      console.log('=================');
+      if (!file.url && !file.preview) {
+        // file.preview = await getBase64(file.originFileObj);
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => {
+          file.preview = reader.result;
+          this.setState({
+            previewImage: file.url || file.preview,
+            previewVisible: true,
+          });
+        };
+      }
+  
+      // this.setState({
+      //   previewImage: file.url || file.preview,
+      //   previewVisible: true,
+      // });
+    };
     getBase64 = (file) => {
-        return new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.readAsDataURL(file);
-          reader.onload = () => resolve(reader.result);
-          reader.onerror = error => reject(error);
-        });
-      }
-      onChange = (evt) => {
-        console.log('--------------')
-        var file = evt.target.files[0];
-        var reader = new FileReader();
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
         reader.readAsDataURL(file);
-        reader.onload = function(e){
-          document.getElementById('imgId').src = e.target.result;
-      }
-      }
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
+    }
     render() {
-        
+      const props = {
+        listType: 'picture',
+        defaultFileList: [...this.state.fileList],
+      };
         return (
-            <div className="pic-container" >
-              <input type="file" id="uploadfile" onChange={this.onChange}></input>
-              <img id="imgId" src=""/>
+            <div className="pictures-wall-container" >
+              <Upload  
+              listType="picture-card"
+              fileList={this.state.fileList}
+              onChange={this.handleChange}>
+              
+              </Upload>
             </div>
         )
     }
